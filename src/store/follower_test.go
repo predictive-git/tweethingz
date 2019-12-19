03b2177e100b8b9ef3a6c905b001507efbd8dc7b
Mark Chmarny <mark@chmarny.com>
2019-12-19 06:05:42 -0800

    multiple account scheduler

--- src/store/follower_test.go
+++ src/store/follower_test.go
@@ -9,13 +9,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func getTestTwitterAccount() string {
+	return os.Getenv("TEST_TW_ACCOUNT")
+}
+
+func TestTwitterTestAccount(t *testing.T) {
+	val := getTestTwitterAccount()
+	t.Logf("val: %s", val)
+	assert.NotEmpty(t, val)
+}
+
 func TestGetDailyFollowerStatesSince(t *testing.T) {
 
 	ctx := context.Background()
-	username := os.Getenv("TW_USER")
 	yesterday := time.Now().AddDate(0, 0, -1)
 
-	data, err := GetDailyFollowerStatesSince(ctx, username, yesterday)
+	data, err := GetDailyFollowerStatesSince(ctx, getTestTwitterAccount(), yesterday)
 	assert.Nil(t, err)
 	assert.NotNil(t, data)
 
